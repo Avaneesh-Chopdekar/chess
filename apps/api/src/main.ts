37219968cf12093748d051076072a0f6fe44fe83
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-09-01 10:30:33 +0530

    setup better auth

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,13 +5,30 @@
 
 import express from 'express';
 import * as path from 'path';
+import { fromNodeHeaders, toNodeHandler } from "better-auth/node";
+import cors from 'cors';
+import { auth } from "./auth";
 
 const app = express();
 
+app.all("/api/auth/*", toNodeHandler(auth));
+
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.use(express.json());
+
+app.use(cors())
 
 app.get('/api/v1/health-check', (req, res) => {
   res.send({ status: 'healthy' });
+});
+
+ 
+app.get("/api/v1/auth/me", async (req, res) => {
+ 	const session = await auth.api.getSession({
+      headers: fromNodeHeaders(req.headers),
+    });
+	return res.json(session);
 });
 
 const port = process.env.PORT || 3001;
